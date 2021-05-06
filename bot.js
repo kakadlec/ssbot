373d9c09866cad9bb31b8113c54322a53c7b1c2c
Karel Kadlec <kakadlec@gmail.com>
2021-05-05 22:31:17 -0300

    initial bot development

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+
+const BOT_TOKEN = process.env.BOT_TOKEN
+
+const bot = new Discord.Client()
+bot.login(BOT_TOKEN)
+
+bot.on('ready', (evt) => {
+  console.log(`Logged in as: ${bot.user.tag}`)
+})
+
+bot.on('message', async msg => {
+  if (msg.content.startsWith('!ola')) {
+    return msg.reply('Seja bem vindo!!')
+  }
+  if (msg.content === 'ping') {
+    msg.channel.send('pong')
+  }
+  if (msg.content === '!git') {
+    msg.reply('Contribua! -> https://github.com/kakadlec/ssbot')
+  }
+})
