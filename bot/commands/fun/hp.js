cc48897cf60c026ba70e6681f661cc92cf788d56
Karel Kadlec <kakadlec@gmail.com>
2021-08-04 16:53:40 -0300

    style: adjust lint

--- bot/commands/fun/hp.js
+++ bot/commands/fun/hp.js
@@ -6,19 +6,19 @@ module.exports = {
   cooldown: 5,
   description: 'hp!',
   args: true,
-  execute(message, args) {
+  execute (message, args) {
     test(message, args)
-  },
+  }
 }
 
 const test = async (message, args) => {
   const res = await axios.get(url, {
     headers: {
       'x-rapidapi-key': 'f4b7f655b0mshb45b5b76db013c1p17a68ejsne0df4f619184',
-      'x-rapidapi-host': 'ajith-messages.p.rapidapi.com',
+      'x-rapidapi-host': 'ajith-messages.p.rapidapi.com'
     }
   }).catch((err) => {
-    return {data: err.message}
+    return { data: err.message }
   })
   message.channel.send(`Hello ${args[0]}, ${res.data.Message}`)
 }
