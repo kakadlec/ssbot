58502e7dc6f0a45e7e7bbe24e76627fbb58d47e0
Karel Kadlec <kakadlec@gmail.com>
2021-08-04 16:06:16 -0300

    fix: deprecated body-json

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,11 +1,11 @@
-const bodyParser = require('body-parser')
+const express = require('express')
 
 const helmet = require('helmet')
 const cors = require('cors')
 const log = require('./log')
 
 module.exports = (app) => {
-  app.use(bodyParser.json())
+  app.use(express.json())
   app.use(helmet())
   app.use(cors())
   app.use(log)
